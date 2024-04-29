e64c8e108edd558f58be84873ffa12bb6bc8d093
JavaRacing <61991037+JavaRacing1@users.noreply.github.com>
2024-04-29 11:03:01 +0200

    Remove generation test and create LevelTileManager

--- script/level/GameLevel.cs
+++ script/level/GameLevel.cs
@@ -1,5 +1,3 @@
-using System;
-
 using Godot;
 
 namespace INTOnlineCoop.Script.Level
@@ -9,6 +7,8 @@ namespace INTOnlineCoop.Script.Level
     /// </summary>
     public partial class GameLevel : Node2D
     {
+        [Export] private LevelTileManager _tileManager;
+
         /// <summary>
         /// Initializes the level instance
         /// </summary>
@@ -19,28 +19,6 @@ namespace INTOnlineCoop.Script.Level
             _ = terrainImage;
 
             GD.Print("GameLevel initialized!");
-        }
-
-        private static void OnGenerationButtonPressed()
-        {
-            //Level generator tests
-            LevelGenerator generator = new();
-            PlayerPositionGenerator positionGenerator = new();
-            generator.EnableDebugMode();
-            foreach (TerrainShape type in Enum.GetValues<TerrainShape>())
-            {
-                generator.SetTerrainShape(type);
-                int seed = new Random().Next();
-                Image image = generator.Generate(seed);
-                positionGenerator.Init(image, type.ToString(), true);
-
-                double positionSeed = new Random().NextDouble();
-                for (int i = 0; i < 5; i++)
-                {
-                    (double, double) pos = positionGenerator.GetSpawnPosition(positionSeed);
-                    GD.Print(pos);
-                }
-            }
         }
     }
 }
---
+++ script/level/LevelTileManager.cs
@@ -0,0 +1,11 @@
+using Godot;
+
+namespace INTOnlineCoop.Script.Level
+{
+    /// <summary>
+    /// Manages the tiles of a level
+    /// </summary>
+    public partial class LevelTileManager : Node2D
+    {
+    }
+}
