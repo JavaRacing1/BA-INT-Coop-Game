e83b6a71113871dcc47406ea689149b3f43c2a52
JavaRacing <61991037+JavaRacing1@users.noreply.github.com>
2024-05-07 10:50:50 +0200

    Add camera zoom

--- script/level/PlayerCamera.cs
+++ script/level/PlayerCamera.cs
@@ -1,3 +1,5 @@
+using System;
+
 using Godot;
 
 namespace INTOnlineCoop.Script.Level
@@ -8,6 +10,10 @@ namespace INTOnlineCoop.Script.Level
     public partial class PlayerCamera : Camera2D
     {
         [Export(PropertyHint.Range, "0,500,")] private int _cameraSpeed = 10;
+        [ExportGroup("Zoom")]
+        [Export(PropertyHint.Range, "0,1,")] private float _zoomSize = 0.1f;
+        [Export(PropertyHint.Range, "0.01,2,")] private float _minZoom = 0.1f;
+        [Export(PropertyHint.Range, "0.01,2,")] private float _maxZoom = 2f;
 
         /// <summary>
         /// Called every frame
@@ -36,6 +42,29 @@ namespace INTOnlineCoop.Script.Level
             }
 
             Position += moveVector * _cameraSpeed;
+        }
+
+        /// <summary>
+        /// Called when an InputEvent occurs
+        /// </summary>
+        /// <param name="event">The input event</param>
+        public override void _UnhandledInput(InputEvent @event)
+        {
+            if (@event is InputEventMouseButton mouseEvent)
+            {
+                int zoomDirectionMultiplier = mouseEvent.ButtonIndex switch
+                {
+                    MouseButton.WheelUp => 1,
+                    MouseButton.WheelDown => -1,
+                    _ => 0
+                };
+
+                if (zoomDirectionMultiplier != 0)
+                {
+                    float newZoom = Math.Clamp(Zoom.X + (_zoomSize * zoomDirectionMultiplier), _minZoom, _maxZoom);
+                    Zoom = new Vector2(newZoom, newZoom);
+                }
+            }
         }
     }
 }
