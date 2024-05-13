523d534364f5554f0d350cba9c1167423fe4f121
JavaRacing <61991037+JavaRacing1@users.noreply.github.com>
2024-05-13 09:06:11 +0200

    Add settings button

--- script/ui/component/PauseDialog.cs
+++ script/ui/component/PauseDialog.cs
@@ -1,6 +1,7 @@
 using Godot;
 
 using INTOnlineCoop.Script.Level;
+using INTOnlineCoop.Script.UI.Screen;
 
 namespace INTOnlineCoop.Script.UI.Component
 {
@@ -9,10 +10,25 @@ namespace INTOnlineCoop.Script.UI.Component
     /// </summary>
     public partial class PauseDialog : Control
     {
+        private SettingsWindow _settingsWindow;
+
         private void OnResumeButtonPressed()
         {
             Visible = false;
             GameLevel.IsInputBlocked = false;
+        }
+
+        private void OnSettingsButtonPressed()
+        {
+            if (_settingsWindow == null)
+            {
+                _settingsWindow = GD.Load<PackedScene>("res://scene/ui/screen/SettingsWindow.tscn")
+                    .Instantiate<SettingsWindow>();
+                AddChild(_settingsWindow);
+            }
+
+            _settingsWindow.Layer = 2;
+            _settingsWindow.Visible = true;
         }
     }
 }
