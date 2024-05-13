475e4d82afe82f7af039ae0294c3b6712e41e619
JavaRacing <61991037+JavaRacing1@users.noreply.github.com>
2024-05-13 08:41:17 +0200

    Add pause dialog

--- script/level/GameLevel.cs
+++ script/level/GameLevel.cs
@@ -1,6 +1,7 @@
 using Godot;
 
 using INTOnlineCoop.Script.Level.Tile;
+using INTOnlineCoop.Script.UI.Component;
 
 namespace INTOnlineCoop.Script.Level
 {
@@ -11,6 +12,7 @@ namespace INTOnlineCoop.Script.Level
     {
         [Export] private LevelTileManager _tileManager;
         [Export] private PlayerCamera _camera;
+        [Export] private CanvasLayer _userInterfaceLayer;
 
         private Image _terrainImage;
 
@@ -39,6 +41,26 @@ namespace INTOnlineCoop.Script.Level
             if (_tileManager != null)
             {
                 _tileManager.InitTileMap(_terrainImage);
+            }
+        }
+
+        /// <summary>
+        /// Called when an InputEvent occurs
+        /// </summary>
+        /// <param name="event">The input event</param>
+        public override void _UnhandledInput(InputEvent @event)
+        {
+            if (@event is InputEventKey { Keycode: Key.Escape } && _userInterfaceLayer != null)
+            {
+                PauseDialog pauseDialog = _userInterfaceLayer.GetNodeOrNull<PauseDialog>("PauseDialog");
+                if (pauseDialog == null)
+                {
+                    pauseDialog = GD.Load<PackedScene>("res://scene/ui/component/PauseDialog.tscn")
+                        .Instantiate<PauseDialog>();
+                    _userInterfaceLayer.AddChild(pauseDialog);
+                }
+
+                pauseDialog.Visible = true;
             }
         }
     }
---
+++ script/ui/component/PauseDialog.cs
@@ -0,0 +1,11 @@
+using Godot;
+
+namespace INTOnlineCoop.Script.UI.Component
+{
+    /// <summary>
+    /// Window when a player presses ESC in a level
+    /// </summary>
+    public partial class PauseDialog : Control
+    {
+    }
+}
