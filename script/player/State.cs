625d0c6d9871ed806f82f44692dba179562a9dc3
Leiti123 <153296765+Leiti123@users.noreply.github.com>
2024-07-23 20:28:07 +0200

    Added Scripts / Classes
    Hinzufügen der Rohdateien für die Klassen. Allerdings ohne Funktion.

--- script/player/State.cs
+++ script/player/State.cs
@@ -2,4 +2,21 @@ using Godot;
 
 public abstract partial class State : Node
 {
+    public StateMachine StateMachine { set; }
+    public virtual void HandleInput(InputEvent @event) 
+    {
+        // Implement this method in derived classes
+    }
+    public virtual void Update(float delta) 
+    {
+        // Implement this method in derived classes
+    }
+    public virtual void PhysicsUpdate(float delta) 
+    {
+        // Implement this method in derived classes
+    }
+    public virtual void Exit() 
+    {
+        // Implement this method in derived classes
+    }
 }
