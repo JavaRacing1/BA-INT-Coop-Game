691444fedc326dd0e1a4c3893430c4a50a8c61c9
JavaRacing <61991037+JavaRacing1@users.noreply.github.com>
2024-11-18 09:10:32 +0100

    Cleanup tutorial code

--- script/ui/screen/GameTutorial.cs
+++ script/ui/screen/GameTutorial.cs
@@ -14,21 +14,24 @@ namespace INTOnlineCoop.Script.UI.Screen
     public partial class GameTutorial : Control
     {
         private static readonly Dictionary<int, Texture2D> TextureCache = new();
-        private readonly string[] _tutorialText =
-            {"Willkommen beim BA INT-Game!\nDiese kleine Einführung soll dem Spieler die grundlegenden Prinzipien, Inhalte und Ziele des Spiels vermitteln.",
-         "",
-         "",
-         "",
-         "",
-         "",
-         "",
-         "" ,
-         ""};
 
-        private int _tutorialTextIndex;
-        private Label _infoText, _pageLabel;
-        private TextureRect _imagePanel;
-        private Button _showNextButton, _showPreviousButton;
+        private readonly string[] _tutorialText =
+        {
+            "Willkommen beim BA INT-Game!\nDiese kleine Einführung soll dem Spieler die grundlegenden Prinzipien, Inhalte und Ziele des Spiels vermitteln.",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            ""
+        };
+
+        [Export] private int _tutorialTextIndex;
+        [Export] private Label _infoText, _pageLabel;
+        [Export] private TextureRect _imagePanel;
+        [Export] private Button _showNextButton, _showPreviousButton;
 
         /// <summary>
         /// Initialisierung Bilder und Textbox mit ersten Eintrag
@@ -36,23 +39,19 @@ namespace INTOnlineCoop.Script.UI.Screen
         /// </summary>
         public override void _Ready()
         {
-            _infoText = GetNode<Label>("InfoTextLabel");
-            _imagePanel = GetNode<TextureRect>("InfoImage");
-            _showPreviousButton = GetNode<Button>("ShowPreviousButton");
-            _showNextButton = GetNode<Button>("ShowNextButton");
-            _pageLabel = GetNode<Label>("PageLabel");
-
             //vorab laden der Seitenhintergründe, um ein langsames Laden beim Seitenwechsel zu vermeiden
             for (int i = 0; i < _tutorialText.Length; i++)
             {
                 string texturePath = $"res://assets/texture/tutorial/Page{i}.png";
                 if (ResourceLoader.Exists(texturePath))
                 {
-                    if (!TextureCache.ContainsKey(i))
+                    if (TextureCache.ContainsKey(i))
                     {
-                        Texture2D texture = GD.Load<Texture2D>(texturePath);
-                        TextureCache.Add(i, texture);
+                        continue;
                     }
+
+                    Texture2D texture = GD.Load<Texture2D>(texturePath);
+                    TextureCache.Add(i, texture);
                 }
                 else
                 {
@@ -70,6 +69,7 @@ namespace INTOnlineCoop.Script.UI.Screen
         {
             _ = GetTree().ChangeSceneToFile("res://scene/ui/screen/MainMenu.tscn");
         }
+
         /// <summary>
         /// Anzeigen der nächsten Seite und Beschreibung
         /// </summary>
@@ -93,19 +93,25 @@ namespace INTOnlineCoop.Script.UI.Screen
                 UpdateUserInterface();
             }
         }
+
         /// <summary>
         /// Wechsel der verschiedenen Bilder und zugehörigen Textabschnitte aus dem Array
         /// </summary>
         private void UpdateUserInterface()
         {
+            if (_showPreviousButton == null || _showNextButton == null || _infoText == null || _pageLabel == null)
+            {
+                return;
+            }
+
             _showPreviousButton.Disabled = _tutorialTextIndex == 0;
             _showNextButton.Disabled = _tutorialTextIndex == (_tutorialText.Length - 1);
 
             _infoText.Text = _tutorialText[_tutorialTextIndex];
             _pageLabel.Text = $"{_tutorialTextIndex + 1}/{_tutorialText.Length}";
 
-            _imagePanel.Texture = TextureCache.ContainsKey(_tutorialTextIndex) ? TextureCache[_tutorialTextIndex] : null;
+            _imagePanel.Texture =
+                TextureCache.ContainsKey(_tutorialTextIndex) ? TextureCache[_tutorialTextIndex] : null;
         }
     }
 }
-
