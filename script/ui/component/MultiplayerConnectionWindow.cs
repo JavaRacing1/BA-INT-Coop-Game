57847341f59e2606508105deb9ed630606b669f3
R4z0rb3ard <DJIceCube09@web.de>
2024-11-18 08:17:19 +0100

    Formatierung fixed

--- script/ui/component/MultiplayerConnectionWindow.cs
+++ script/ui/component/MultiplayerConnectionWindow.cs
@@ -2,8 +2,8 @@ using System;
 
 using Godot;
 
-using INTOnlineCoop.Script.Singleton;
 using INTOnlineCoop.Script.Player;
+using INTOnlineCoop.Script.Singleton;
 
 namespace INTOnlineCoop.Script.UI.Component
 {
